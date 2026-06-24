c75a8cbc58d6435cedebf897d22fc1f50431b058
nuuuwan <nuuuwan@gmail.com>
2026-06-24 09:32:00 +0530

    style: Refactor logging and formatting in TableLoaderMixin

--- src/census/table/TableLoaderMixin.py
+++ src/census/table/TableLoaderMixin.py
@@ -1,20 +1,36 @@
 from utils_future import Log
 
-log = Log('TableLoaderMixin')
+log = Log("TableLoaderMixin")
 
 
 class TableLoaderMixin:
     @classmethod
     def from_dict(cls, d):
         return cls(
-            table_no=d['table_no'],
-            table_name=d['table_name'],
-            doc_page_no=d['doc_page_no'],
+            table_no=d["table_no"],
+            table_name=d["table_name"],
+            doc_page_no=d["doc_page_no"],
         )
+
+    @classmethod
+    def validate_tables(cls, tables):
+        prev_doc_page_no = None
+        for table in tables:
+            if (
+                prev_doc_page_no is not None
+                and table.doc_page_no < prev_doc_page_no
+            ):
+                raise ValueError(
+                    f"Table {table.table_no}"
+                    + f" has doc_page_no {table.doc_page_no}, "
+                    + "which is less than than previous table's"
+                    + f" doc_page_no {prev_doc_page_no}"
+                )
+            prev_doc_page_no = table.doc_page_no
 
     @classmethod
     def list(cls):
         metadata = cls.load_metadata()
         tables = [cls.from_dict(d) for d in metadata]
-        log.debug(f'Loaded {len(tables)} tables')
+        log.debug(f"Loaded {len(tables)} tables")
         return tables
