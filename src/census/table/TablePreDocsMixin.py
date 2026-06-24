101bd037a2e0067a79fb5174d34021bafd8ddf02
nuuuwan <nuuuwan@gmail.com>
2026-06-24 13:47:30 +0530

    feat: Add methods to extract title and index pages from cleaned original PDF

--- src/census/table/TablePreDocsMixin.py
+++ src/census/table/TablePreDocsMixin.py
@@ -18,6 +18,14 @@ class TablePreDocsMixin:
     LIST_OF_TABLES_FROM_PAGE = 8
     LIST_OF_TABLES_TO_PAGE = 11
 
+    TITLE_PAGE_PDF_FILE = PDFFile(
+        os.path.join("data", "lk_census1971.title_page.pdf")
+    )
+
+    INDEX_PAGE_PDF_FILE = PDFFile(
+        os.path.join("data", "lk_census1971.index_page.pdf")
+    )
+
     @classmethod
     def clean_original_report(cls):
         if cls.CLEANED_ORIGINAL_PDF_FILE.exists:
@@ -29,6 +37,17 @@ class TablePreDocsMixin:
             page_numbers, cls.CLEANED_ORIGINAL_PDF_FILE
         )
         log.info(f"Wrote {cls.CLEANED_ORIGINAL_PDF_FILE}.")
+
+    @classmethod
+    def extract_title_page(cls):
+        if cls.TITLE_PAGE_PDF_FILE.exists:
+            log.debug(f"{cls.TITLE_PAGE_PDF_FILE} exists.")
+            return
+
+        cls.CLEANED_ORIGINAL_PDF_FILE.extract_pages(
+            [0], cls.TITLE_PAGE_PDF_FILE
+        )
+        log.info(f"Wrote {cls.TITLE_PAGE_PDF_FILE}.")
 
     @classmethod
     def extract_list_of_tables(cls):
@@ -48,6 +67,19 @@ class TablePreDocsMixin:
         log.info(f"Wrote {cls.LIST_OF_TABLES_PDF_FILE}.")
 
     @classmethod
+    def extract_index_page(cls):
+        if cls.INDEX_PAGE_PDF_FILE.exists:
+            log.debug(f"{cls.INDEX_PAGE_PDF_FILE} exists.")
+            return
+
+        cls.LIST_OF_TABLES_PDF_FILE.extract_pages(
+            [0], cls.INDEX_PAGE_PDF_FILE
+        )
+        log.info(f"Wrote {cls.INDEX_PAGE_PDF_FILE}.")
+
+    @classmethod
     def build_pre_docs(cls):
         cls.clean_original_report()
+        cls.extract_title_page()
         cls.extract_list_of_tables()
+        cls.extract_index_page()
