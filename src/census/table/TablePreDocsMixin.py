a41f67cd804ffd68aa938005b57ce66e11dfe290
nuuuwan <nuuuwan@gmail.com>
2026-06-24 14:16:39 +0530

    refactor: Consolidate LIST_OF_TABLES page range and reorder methods in TablePreDocsMixin

--- src/census/table/TablePreDocsMixin.py
+++ src/census/table/TablePreDocsMixin.py
@@ -15,13 +15,10 @@ class TablePreDocsMixin:
     LIST_OF_TABLES_PDF_FILE = PDFFile(
         os.path.join("data", "list_of_tables.pdf")
     )
-    LIST_OF_TABLES_FROM_PAGE = 8
-    LIST_OF_TABLES_TO_PAGE = 11
-
+    LIST_OF_TABLES_FROM_PAGE, LIST_OF_TABLES_TO_PAGE = 8, 11
     TITLE_PAGE_PDF_FILE = PDFFile(
         os.path.join("data", "lk_census1971.title_page.pdf")
     )
-
     INDEX_PAGE_PDF_FILE = PDFFile(
         os.path.join("data", "lk_census1971.index_page.pdf")
     )
@@ -31,7 +28,6 @@ class TablePreDocsMixin:
         if cls.CLEANED_ORIGINAL_PDF_FILE.exists:
             log.debug(f"{cls.CLEANED_ORIGINAL_PDF_FILE} exists.")
             return
-
         page_numbers = list(range(0, 56)) + list(range(58, 231))
         cls.ORIGINAL_PDF_FILE.extract_pages(
             page_numbers, cls.CLEANED_ORIGINAL_PDF_FILE
@@ -43,7 +39,6 @@ class TablePreDocsMixin:
         if cls.TITLE_PAGE_PDF_FILE.exists:
             log.debug(f"{cls.TITLE_PAGE_PDF_FILE} exists.")
             return
-
         cls.CLEANED_ORIGINAL_PDF_FILE.extract_pages(
             [0], cls.TITLE_PAGE_PDF_FILE
         )
@@ -57,7 +52,6 @@ class TablePreDocsMixin:
         if cls.LIST_OF_TABLES_PDF_FILE.exists:
             log.debug(f"{cls.LIST_OF_TABLES_PDF_FILE} exists.")
             return
-
         cls.CLEANED_ORIGINAL_PDF_FILE.extract_pages(
             list(
                 range(
@@ -79,16 +73,8 @@ class TablePreDocsMixin:
             [0], cls.INDEX_PAGE_PDF_FILE
         )
         log.info(f"Wrote {cls.INDEX_PAGE_PDF_FILE}.")
-
         cls.INDEX_PAGE_PDF_FILE.extract_images()
         log.info(f"Wrote images from {cls.INDEX_PAGE_PDF_FILE}.")
-
-    @classmethod
-    def build_pre_docs(cls):
-        cls.clean_original_report()
-        cls.extract_title_page()
-        cls.extract_list_of_tables()
-        cls.extract_index_page()
 
     @classmethod
     def get_title_page_first_image_file(cls):
@@ -105,3 +91,10 @@ class TablePreDocsMixin:
                 cls.INDEX_PAGE_PDF_FILE.path[:-4] + ".images", "image-01.png"
             )
         )
+
+    @classmethod
+    def build_pre_docs(cls):
+        cls.clean_original_report()
+        cls.extract_title_page()
+        cls.extract_list_of_tables()
+        cls.extract_index_page()
