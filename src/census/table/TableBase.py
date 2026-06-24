020b8f6e98af2191d6a308f57d43a2ec1e71d88d
nuuuwan <nuuuwan@gmail.com>
2026-06-24 08:52:20 +0530

    feat: Add cached_property decorator to table_id method for optimized access

--- src/census/table/TableBase.py
+++ src/census/table/TableBase.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from functools import cached_property
 
 
 @dataclass
@@ -6,3 +7,10 @@ class TableBase:
     table_no: str
     table_name: str
     doc_page_no: int
+
+    @cached_property
+    def table_id(self) -> str:
+        part1, part2 = self.table_no.split(".")
+        part1 = int(part1)
+        part2 = int(part2)
+        return f"table-{part1:02d}-{part2:02d}"
