76eee2a884aff8b8f1003fd132bf9d5e4a9e62a1
nuuuwan <nuuuwan@gmail.com>
2026-06-24 09:45:04 +0530

    feat: Implement data_file property and build_data method in TableDataMixin

--- src/census/table/TableDataMixin.py
+++ src/census/table/TableDataMixin.py
@@ -1,3 +1,22 @@
+import os
+from functools import cached_property
+
+from utils_future import JSONFile, Log
+
+log = Log("TableDataMixin")
+
+
 class TableDataMixin:
-    def build_data(self):
-        pass
+    @cached_property
+    def data_file(self):
+        return JSONFile(os.path.join(self.dir_data, "data.json"))
+
+    def extract_data_with_ai(self):
+        return {}
+
+    def build_data(self, force=True):
+        if self.data_file.exists and not force:
+            return
+        data = self.extract_data_with_ai()
+        self.data_file.write(data)
+        log.info(f"Wrote {self.data_file}.")
