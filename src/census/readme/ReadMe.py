dd5125666a2cf4ca36e521322bde34016064ecef
nuuuwan <nuuuwan@gmail.com>
2026-06-24 12:07:14 +0530

    fix: Improve table listing format in ReadMe by enhancing readability

--- src/census/readme/ReadMe.py
+++ src/census/readme/ReadMe.py
@@ -36,10 +36,12 @@ class ReadMe:
         for group_id, group_tables in tables_by_group.items():
             lines.extend([f"### {group_id[6:]}.x", ""])
             for table in group_tables:
-                lines.append(f"- {
+                lines.append(
+                    f"- {
                         table.table_no}: [{
                         table.table_name}]({
-                        table.dir_data})")
+                        table.dir_data})"
+                )
             lines.append("")
         return lines
 
